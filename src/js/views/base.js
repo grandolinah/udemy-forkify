61114aaf73cb3b7796e87e0984e495f8a76fb601
Ani Jekova <anjekova@gmail.com>
2020-01-30 09:37:45 +0200

    search model and view

---
+++ src/js/views/base.js
@@ -0,0 +1,12 @@
+export const elements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  searchRes: document.querySelector('.results'),
+  searchResList: document.querySelector('.results__list'),
+  searchResPages: document.querySelector('.results__pages'),
+  recipe: document.querySelector('.recipe'),
+};
+
+export const elementStrings = {
+  loader: 'loader'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/js/index.js',
@@ -9,22 +8,5 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
   }
 };
