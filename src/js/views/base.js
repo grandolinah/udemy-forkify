6fd838a82f46ca299a649bbdd38d30bec2635331
Ani Jekova <anjekova@gmail.com>
2020-01-30 09:47:19 +0200

    search model and view

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -10,3 +10,19 @@ export const elements = {
 export const elementStrings = {
   loader: 'loader'
 };
+
+export const renderLoader = parent => {
+  const loader = `
+      <div class="${elementStrings.loader}">
+          <svg>
+              <use href="img/icons.svg#icon-cw"></use>
+          </svg>
+      </div>
+  `;
+  parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
+};
