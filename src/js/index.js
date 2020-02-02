1c4d992957b68ae0e4b35ba08df7f961f4ab2b36
Ani Jekova <anjekova@gmail.com>
2020-02-02 12:17:08 +0200

    list model added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, clearLoader, renderLoader } from './views/base';
@@ -115,3 +116,6 @@ elements.recipe.addEventListener('click', e => {
   }
   console.log(state.recipe);
 })
+
+
+window.l = new List();
